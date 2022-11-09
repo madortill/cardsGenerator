07792730b6f806cc9b3cda3329ecad78fce1b5d8
madortill <mador.till@gmail.com>
2022-11-09 18:47:39 +0200

    add title input

--- src/components/MainStage.vue
+++ src/components/MainStage.vue
@@ -2,7 +2,13 @@
     <div>
         <ColorPicker></ColorPicker>
         <div class="flex-container" :style = "{backgroundImage: 'url(src/assets/cardsAssets/colors/' + this.theme + '/images/general/bg.svg)'}">
-            <input class="title-input" type="text" placeholder="שם הלומדה"/>
+            <div class="input-container">
+                <input class="title-input" type="text" placeholder="שם הלומדה"/>
+            </div>
+            <div class="learningCard" :style="{backgroundImage: 'url(src/assets/cardAssets/colors/' + this.theme + '/images/learning/subject_btn.svg)'}">
+                <img :src="'src/assets/cardsAssets/colors/' + this.theme + '/images/learning/Artboard 4.svg'" class="icon">
+                <div class="subject">הכנס שם של נושא</div>
+            </div>
         </div>
     </div>
 </template>
@@ -25,20 +31,58 @@ export default {
 
 <style scoped>
     .title-input {
-        border-radius: 3rem;
-        font-size: 2rem;
+        /* background-image:url("src/assets/colorNeutralAssets/home_header.svg");
+        background-repeat: no-repeat;
+        background-size: 100% 100%; */
+        font-size: 1.7rem;
         border: none;
-        padding-right: 0.25rem;
-        margin: auto;
-        background-image:url("src/assets/colorNeutralAssets/home_header.svg");
+        background-color: transparent;
+        text-align: center;
+        /* padding: 0.5rem 1rem 1rem; */
+        /* width: 20rem; */
+        padding: 0.6rem 1.3rem 0.8rem;
+        margin-top: 0.5rem;
+        border-radius: 0.8rem;
     }
+    
+    .input-container {
+        background-image:url("src/assets/colorNeutralAssets/home_header.svg");
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        padding: 3rem 0rem 2rem 2rem;
+        width: 20rem;
+        height: fit-content;
+    }
+
+    /* .title-input:focus {
+        padding: 0.6rem 1rem 1rem 1rem;
+    } */
 
     .flex-container {
         display: flex;
+        flex-direction: column;
+        align-items: center;
         height: 100vh;
         width: 66.67vw;
         position: absolute;
         left: 0;
-        /* background-image: url(@/assets/cards-assets/colors/lightBlue/images/general/bg.svg) */
+    }
+
+    .learningCard {
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+        height: 12em;
+        width: 17em;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-size: 4.5vmin;
+    }
+
+    .icon {
+        width: 2em;
+        position: relative;
+        top: 3em;
+        left: 0.4em;
     }
 </style>
