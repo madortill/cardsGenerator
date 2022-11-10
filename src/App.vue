4b83018e9d617e9da5085e7f9a3e3f2714ae9809
madortill <mador.till@gmail.com>
2022-11-10 12:00:28 +0200

    backup before weekend

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import Instructions from './components/Instructions.vue'
 import OpenScreen from './components/OpenScreen.vue'
-import MainStage from './components/MainStage.vue'
+import MainStage from './components/MainScreen.vue'
 
 
     export default {
@@ -9,7 +9,98 @@ import MainStage from './components/MainStage.vue'
       data() {
         return {
           currentStage: 2,
-          theme: "lightBlue"
+          theme: "lightBlue",
+          cardsData: {
+                "title1": {
+                    "icon": `../assets/images//learning/Artboard 4.svg`,
+                    "amountOfQuestions": 2,
+                    "questionsExam": [
+                        {
+                            type: "multiple",
+                            question: "דוגמה לשאלה אמריקאית מתרגלת שיש לה 4 תשובות?",
+                            ans1: "תשובה ראשונה ולא נכונה",
+                            ans2: "תשובה שנייה נכונה!",
+                            ans3: "תשובה שלישית ולא נכונה",
+                            ans4: "תשובה רביעית לא נכונה",
+                            correctAns: "ans2"
+                        },
+                        {
+                            type: "binary",
+                            sentence: "דוגמה לשאלה מתרגלת נכונה של נכון או לא נכון",
+                            trueOrFalse: true
+                        }
+                    ],
+                    "questionsPractice": [
+                        {
+                            type: "multiple",
+                            question: "דוגמה לשאלה אמריקאית בוחנת שיש לה 4 תשובות?",
+                            ans1: "תשובה ראשונה ולא נכונה",
+                            ans2: "תשובה שנייה נכונה!",
+                            ans3: "תשובה שלישית ולא נכונה",
+                            ans4: "תשובה רביעית לא נכונה",
+                            correctAns: "ans2"
+                        },
+                        {
+                            type: "binary",
+                            sentence: "דוגמה לשאלה בוחנת נכונה של נכון או לא נכון",
+                            trueOrFalse: true
+                        }
+                    ],
+                    "learningContent": {
+                        "תת נושא 1": {
+                            "כרטיסייה ראשונה מסוג טקסט": [
+                                {
+                                    cardType: "youtube",
+                                    // Make sure it is embed video!
+                                    youtube: "https://www.youtube.com/embed/n0t9iFlGO20",
+                                    //  https://www.youtube.com/embed/n0t9iFlGO20
+                                    content: "דוגמה לתוכן של כרטיסייה המחולקת לפי נושאים ותתי נושאים."
+                                },
+                            ],
+                            "כרטיסייה שנייה": [
+                                {
+                                    cardType: "picAndText",
+                                    pic: "../assets/images/pic.png",
+                                    // content: "דוגמה לכותרת לתמונה הראשונה",
+                                }
+                            ],
+                        },
+                        "תת נושא 2": {
+                            "כרטיסייה שלישית מסוג וידאו": [
+                                {
+                                    cardType: "videoAndText",
+                                    video: "../assets/images/פתיח.mp4",
+                                    content: "סרטון מוסר שלא הבנתי למה הוא היה על המחשב של פלג... פטל.. שלג?"
+                                }
+                            ],
+                            "כרטיסייה רביעית": [
+                                {
+                                    cardType: "text",
+                                    content: "g"
+                                }
+                            ]
+                        },
+                        "תת נושא 3": {
+                            "תת תת נושא 12": [
+                                {
+                                    cardType: "videoAndText",
+                                    video: "../assets/images/פתיח.mp4",
+                                    content: "סרטון מוסר שלא הבנתי למה הוא היה על המחשב של פלג... פטל.. שלג?"
+                                }
+                            ],
+                        },
+                        "תת נושא 5": {
+                            "תת תת נושא 12": [
+                                {
+                                    cardType: "videoAndText",
+                                    video: "../assets/images/פתיח.mp4",
+                                    content: "סרטון מוסר שלא הבנתי למה הוא היה על המחשב של פלג... פטל.. שלג?"
+                                }
+                            ],
+                        }
+                    },
+                }
+            },
         }
       },
     }
@@ -20,7 +111,9 @@ import MainStage from './components/MainStage.vue'
   <div id="app"> 
     <open-screen @next-stage="currentStage++" v-if="currentStage === 0"></open-screen>
     <instructions v-else-if="currentStage === 1"></instructions>
-    <main-stage v-else-if="currentStage === 2" :theme="theme"></main-stage>
+    <main-stage v-else-if="currentStage === 2" :theme="theme" :subjectArray="Object.keys(this.cardsData)"></main-stage>
+    <!-- <div>{{ cardsData }}</div> -->
+    <!-- <div>{{Object.keys(this.cardsData)}}</div> -->
   </div>
 </template>
 
---
+++ src/components/MainScreen.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+        <ColorPicker></ColorPicker>
+        <div>{{subjectArray}}</div>
+        <div class="flex-container"
+            :style="{ backgroundImage: 'url(src/assets/cardsAssets/colors/' + this.theme + '/images/general/bg.svg)' }">
+            <div class="input-container">
+                <input v-model="title" class="title-input" type="text" placeholder="שם הלומדה" @input="$emit('titleChange', this.title)" />
+            </div>
+            <div class="learningCard"
+                :style="{ backgroundImage: 'url(src/assets/cardsAssets/colors/' + this.theme + '/images/learning/subject_btn.svg)' }">
+                <div class="subject">הוספת נושא</div>
+                <div :style="{ backgroundImage: 'url(src/assets/cardsAssets/colors/' + this.theme + '/images/learning/Artboard_4.svg)' }" class="icon">
+                    <img src="src\assets\colorNeutralAssets\plus-small.svg" class="plus"/>
+                </div>
+            </div>
+            <!-- <div class="learningCard"
+                :style="{ backgroundImage: 'url(src/assets/cardsAssets/colors/' + this.theme + '/images/learning/subject_btn.svg)' }"
+                v-for="(value, index) in subjectArray" :key="'title' + index">
+                <div class="subject">{{value}}</div>
+                <div :style="{ backgroundImage: 'url(src/assets/cardsAssets/colors/' + this.theme + '/images/learning/Artboard_4.svg)' }" class="icon">
+                    <img src="src\assets\colorNeutralAssets\plus-small.svg" class="plus"/>
+                </div>
+            </div> -->
+            </div>
+        </div>
+</template>
+
+<script>
+import ColorPicker from './ColorPicker.vue';
+
+export default {
+    name: "main-stage",
+    data() {
+        return {
+            title: ""
+        };
+    },
+    component: {},
+    props: ["subjectArray", "theme"],
+    methods: {},
+    components: { ColorPicker },
+}
+
+</script>
+
+<style scoped>
+.title-input {
+    font-size: 1.7rem;
+    border: none;
+    background-color: transparent;
+    text-align: center;
+    padding: 0.6rem 1.3rem 0.8rem;
+    margin-top: 0.5rem;
+    border-radius: 0.8rem;
+}
+
+.input-container {
+    background-image: url("src/assets/colorNeutralAssets/home_header.svg");
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    padding: 3rem 0rem 2rem 2rem;
+    width: 20rem;
+    height: fit-content;
+}
+
+.flex-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 66.67vw;
+    position: absolute;
+    left: 0;
+}
+
+.learningCard {
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    height: 17rem;
+    width: 26rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    text-align: center;
+}
+
+.icon {
+    width: 1.6em;
+    height: 1.6em;
+    /* position: relative; */
+    top: 3em;
+    left: 0.4em;
+    margin-top: 2rem;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.plus {
+    width: 1.5em;
+}
+</style>
