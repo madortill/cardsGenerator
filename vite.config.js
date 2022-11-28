b3440b778bf68921eabbdae086f233c01ea83d48
madortill <mador.till@gmail.com>
2022-11-28 14:27:34 +0200

    fix repo name

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === "production" ? "/CardsGenerator/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/cardsGenerator/" : "/",
 
 })
