8ea18139c970310c16aa4605deb924ea1f9d3118
madortill <mador.till@gmail.com>
2023-01-02 08:37:52 +0200

    change deploy method

--- deploy.ps1
+++ deploy.ps1
@@ -5,29 +5,13 @@ write-output $repoURL
 if($repoURL -eq $null -or $repoURL -eq "")
 {
     $repoURL = Read-Host -Prompt "Repository URL not found. Please enter repository url to continue or ctrl + C to quit"
+    git remote add origin $repoURL
 }
 
-npm run build
-
-# navigate into the build output directory
-cd dist
-
-# place .nojekyll to bypass Jekyll processing
-echo "" > .nojekyll
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git checkout -B master
 git add -A
 git commit -m $commitMessage
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f $repoURL master:gh-pages
-
-cd ..
+git push $repoURL 
 
 write-host ""
-write-host "deployed to github"
+write-host "Uploaded to github"
 write-host ""
