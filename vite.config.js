b696e404a4b8426c0f817b8b4d904a30dbf447be
madortill <mador.till@gmail.com>
2024-07-16 12:04:44 +0300

    add legacy support

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 
+import legacy from '@vitejs/plugin-legacy'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +21,9 @@ export default defineConfig({
   },
   plugins: [
     vue({}),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    })
   ],
   base: process.env.NODE_ENV === "production" ? "./" : "/",
 
